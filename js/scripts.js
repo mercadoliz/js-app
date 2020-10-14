3075c0af9da40bc6afe0ebb3bc53060b800dd82b
mercadoliz <mercadoliz08@gmail.com>
2020-10-13 19:26:34 -0700

    Added loop to describe pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
 let pokemonList = [
-    {
-      name: "vulpix",
-      height: .06,
-      types: ["flash fire", "drought"],
-      
-    },
-    {
-      name: "charmander",
-      height: .06,
-      types: ["blaze", "solar power"],
-      
-    },
-    {
-      name: "ponyta",
-      height: 1,
-      types: ["flash fire", "flame body", "runaway"],
-          
-    },
+  {
+    name: "vulpix",
+    height: 0.06,
+    types: ["flash fire", "drought"],
+  },
+  {
+    name: "charmander",
+    height: 0.06,
+    types: ["blaze", "solar power"],
+  },
+  {
+    name: "ponyta",
+    height: 1,
+    types: ["flash fire", "flame body", "runaway"],
+  },
+];
+
+]
+//list all pokemon's name, height, and types
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write("#" + pokemonList[i].name + " " + pokemonList[i].height + "");
+ //creates conditional that if a pokemon is a certain height a phrase will be added on
+    if (pokemonList[i].height >= 1.0) {
+        document.write("(height: " + pokemonList[i].height + ") - Wow that's short!</br>")
+    }
+    else if (pokemonList[i].height < 1.0)
+        document.write("(height: " + pokemonList[i].height + ")</br>")
+};
