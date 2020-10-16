329014ce56e206ab40f3ec0d57925772a8798008
mercadoliz <mercadoliz08@gmail.com>
2020-10-16 13:53:42 -0700

    Added pokemonRepository function to js

--- js/scripts.js
+++ js/scripts.js
@@ -16,14 +16,31 @@ let pokemonList = [
   },
 ];
 
-]
 //list all pokemon's name, height, and types
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write("#" + pokemonList[i].name + " " + pokemonList[i].height + "");
- //creates conditional that if a pokemon is a certain height a phrase will be added on
-    if (pokemonList[i].height >= 1.0) {
-        document.write("(height: " + pokemonList[i].height + ") - Wow that's short!</br>")
-    }
-    else if (pokemonList[i].height < 1.0)
-        document.write("(height: " + pokemonList[i].height + ")</br>")
-};
+  document.write("#" + pokemonList[i].name + " " + pokemonList[i].height + "");
+  //creates conditional that if a pokemon is a certain height a phrase will be added on
+  if (pokemonList[i].height >= 1.0) {
+    document.write(
+      "(height: " + pokemonList[i].height + ") - Wow that's HUGE!</br>"
+    );
+  } else if (pokemonList[i].height < 1.0)
+    document.write("(height: " + pokemonList[i].height + ")</br>");
+}
+
+let pokemonRepository = (function () {
+  let pokemonList = [];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
